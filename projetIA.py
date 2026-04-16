afde9e3d02cd1a84f14bcbe4c42e4d117af3f616
El Mahi Izikki Yassin <24115@ecam.be>
2026-04-16 15:39:15 +0200

    Premiers matchs

--- projetIA.py
+++ projetIA.py
@@ -2,24 +2,33 @@ import socket
 import json
 import struct
 
+
 def send_message(sock, message):
     data = json.dumps(message).encode('utf-8') # transforme en bytes
-    size = struct.pack('!I', len(data))        # taille en binaire
+    size = struct.pack('I', len(data))        # taille en binaire
     sock.send(size + data)                     # envoie taille + message
+    print("sent")
 
 def receive_message(sock):
     raw_size = b''                   # on commence avec rien (b'' = bytes vide)
     while len(raw_size) < 4:         # Lire exactement 4 bytes
         raw_size += sock.recv(4 - len(raw_size))
-    size = struct.unpack('!I', raw_size)[0]  # convertit en nombre
+    size = struct.unpack('I', raw_size)[0]  # convertit en nombre
     data = b'' 
     while len(data) < size:
         data += sock.recv(size - len(data))        # lit exactement 'size' bytes
     return json.loads(data.decode('utf-8'))  # transforme en dictionnaire python
 
-client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+Ping_port = 3000
 
-client.connect(('192.168.0.103', 3000))
+ping_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ping_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+ping_sock.bind(('0.0.0.0', 3000))
+ping_sock.listen(1)
+
+
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect(('172.17.10.46', 3000))
 
 message = {
   "request": "subscribe",
@@ -31,3 +40,26 @@ message = {
 send_message(client, message)
 response = receive_message(client)
 print(response)
+
+
+
+while True:
+    print("Serveur connécté")
+    ping_sock.settimeout(10)
+    while 1:
+        try:
+            conn, addr = ping_sock.accept()
+            break
+        except socket.timeout:
+            print("waiting for request")
+    message = receive_message(conn)
+    print(message)
+
+    if message['request'] == 'ping':
+        send_message(conn, {'response': 'pong'})
+
+    if message['request'] == 'play':
+        send_message(conn, {
+            'response': 'move',
+            'move': 0  
+        })
